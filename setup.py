c67fdf2e10864c83e2f08ea7d987928de7aa5e3d
Parth Sharma <86726240+parth-deepsource@users.noreply.github.com>
2025-04-18 21:14:12 +0530

    chore: add assertion

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ import re, ast
 # get version from __version__ variable in frappe/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
+assert _version_re
+
 with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
 
